51f2f34d8277cd80334922a5cd08ad88ee06c81c
Mensun <mensun@bu.edu>
2025-02-18 15:13:47 -0500

    added error checking

--- pa1part1/client.py
+++ pa1part1/client.py
@@ -7,19 +7,28 @@ try:
   serverPort = int(sys.argv[2])
 except:
   print("Error recieving host & server port (did you input a host & port number?)")
+  sys.exit(1)
 
-# create client socket
-clientSocket = socket(AF_INET, SOCK_STREAM)
+try:
+  # create client socket
+  clientSocket = socket(AF_INET, SOCK_STREAM)
 
-# connect socket
-clientSocket.connect((host, serverPort))
-print(f"Connected to {host}:{serverPort}")
+  # connect socket
+  clientSocket.connect((host, serverPort))
+  print(f"Connected to {host}:{serverPort}")
+except Exception as e:
+  print(f"Error connecting client socket to server: {e}")
+  sys.exit(1)
 
-# send from client socket to server's socket
-message = "Hello on the other side"
-encodedData = message.encode("utf-8")
-clientSocket.send(encodedData)
-echoMessage = clientSocket.recv(2048)
-print(f"Recieved from server: {echoMessage.decode("utf-8")}")
+try:
+  # send from client socket to server's socket
+  message = "Hello on the other side"
+  encodedData = message.encode("utf-8")
+  clientSocket.send(encodedData)
+  echoMessage = clientSocket.recv(2048)
+  print(f"Recieved from server: {echoMessage.decode("utf-8")}")
 
-clientSocket.close()
+  clientSocket.close()
+except Exception as e:
+  print(f"Error sending/receiving from server socket: {e}")
+  sys.exit(1)
--- pa1part1/server.py
+++ pa1part1/server.py
@@ -9,27 +9,35 @@ except Exception as e:
   print("Error:", e)
   sys.exit(1)
 
-# create server socket
-serverSocket = socket(AF_INET, SOCK_STREAM)
-host = "127.0.0.1"
-serverSocket.bind((host, serverPort))
+try:
+  # create server socket
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  host = "127.0.0.1"
+  serverSocket.bind((host, serverPort))
+except Exception as e:
+  print(f"Error creating server socket: {e}")
+  sys.exit(1)
 
-# listen to connections
-serverSocket.listen(1)
-print(f"Le server is now listening on host {host}:{serverPort}")
-while True:
+try:
+  # listen to connections
+  serverSocket.listen(5)
+  print(f"Le server is now listening on host {host}:{serverPort}")
+  while True:
 
-  # accept socket connection
-  newSocket, address = serverSocket.accept()
+    # accept socket connection
+    newSocket, address = serverSocket.accept()
 
-  # use connection to recieve data ('with' handles socket closure)
-  with newSocket:
-    print(f"Le server is now connected to {address}")
-    while True:
-      message = newSocket.recv(2028)
-      # no msg means client dc'd
-      if not message:
-        print("Client dc'd")
-        break
-      decodedMessage = message.decode('utf-8')
-      newSocket.send(decodedMessage.encode())
+    # use connection to recieve data ('with' handles socket closure)
+    with newSocket:
+      print(f"Le server is now connected to {address}")
+      while True:
+        message = newSocket.recv(2028)
+        # no msg means client dc'd
+        if not message:
+          print("Client dc'd")
+          break
+        decodedMessage = message.decode('utf-8')
+        newSocket.send(decodedMessage.encode())
+except Exception as e:
+  print(f"Error listening/communicating with client socket: {e}")
+  sys.exit(1)
